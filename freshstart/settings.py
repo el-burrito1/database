1838c03135169c794ec4d91306c1b9c081b4ab73
Spencer <spencer.spiegel@gmail.com>
2017-11-15 12:19:17 -0800

    changes

--- freshstart/settings.py
+++ freshstart/settings.py
@@ -81,7 +81,16 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'] =  dj_database_url.config()
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+# DATABASES['default'] =  dj_database_url.config()
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE':
+#     }
+# }
 
 
 # Password validation
