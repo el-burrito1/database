ebb2bea879f3f4ac24deabb87bc5cfeec7f3fc3a
Spencer <spencer.spiegel@gmail.com>
2017-11-15 11:09:47 -0800

    update database

--- freshstart/settings.py
+++ freshstart/settings.py
@@ -76,17 +76,12 @@ WSGI_APPLICATION = 'freshstart.wsgi.application'
 
 import dj_database_url
 DATABASES = {
-    
-}
-
-['default'] =  dj_database_url.config()
-
-DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Password validation
