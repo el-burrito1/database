7a4ddf75ebe857e67de5f47c886790ef1c25c00d
Spencer <spencer.spiegel@gmail.com>
2017-11-15 11:02:16 -0800

    updated settings

--- freshstart/settings.py
+++ freshstart/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'freshstart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
